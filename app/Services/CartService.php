c4dbb3e391c4008f82acb5f4249b7c5a8a591aae
ngelashvili <106074010+ngelashvili@users.noreply.github.com>
2023-11-10 14:47:18 +0400

    Update CartService.php

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -43,38 +43,4 @@ class CartService
 
     }
 
-//    public function calculateCartDiscountOld($cart) {
-//        $result = [];
-//        $cartItemsArray = $cart->pluck('quantity', 'product_id')->toArray();
-//
-//        $discountGroups = UserProductGroup::with('productGroupItem')->get();
-//        $matchingGroups = $discountGroups->filter(function ($group) use ($cartItemsArray, &$result) {
-//            $groupProductIds = $group->productGroupItem->pluck('product_id')->toArray();
-//            $hasCombination = count(array_intersect(array_keys($cartItemsArray), $groupProductIds)) == count($groupProductIds);
-//
-//            if($hasCombination) {
-//                $res = array_intersect_key($cartItemsArray, array_flip($groupProductIds));
-//                asort($res);
-//                $result[] = [
-//                    'discount' => $group->discount,
-//                    'discountQuantity' => reset($res),
-//                    'productsIds' => array_keys($res)
-//                ];
-//            }
-//            return $hasCombination;
-//        });
-//
-//        if(!$result) return null;
-//
-//        $minValue = PHP_INT_MAX;
-//        foreach ($result as $item) {
-//            if ($item['discount'] < $minValue) {
-//                $minValue = $item['discount'];
-//                $minDiscount = $item;
-//            }
-//        }
-//
-//        return $minDiscount;
-//
-//    }
 }
